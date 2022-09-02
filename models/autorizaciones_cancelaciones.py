d78d9c50bfac2d2c9cd6e008711c23068101da4e
unknown <alexiz.54@hotmail.com>
2022-09-02 16:45:57 -0600

    CXP AUTORIZACIONES MODULE

--- models/autorizaciones_cancelaciones.py
+++ models/autorizaciones_cancelaciones.py
@@ -12,5 +12,13 @@ class AutorizacionesCancelaciones(models.Model):
 
     name = fields.Many2one('res.users',string='Solicitante',default=lambda self: self.env.user,readonly=True)
 
-    invoice = fields.Many2one('account.move',string='Factura a cancelar')
+    invoice = fields.Many2one('account.move',string='Factura a cancelar',
+                              domain = [('move_type', '=', 'in_invoice'),('payment_state','!=','not_paid')]
+                              ,context="{'tree_view_ref':'account.view_in_invoice_tree'}")
 
+    cancel_motive = fields.Selection(selection=[
+        ('bad_invoice', 'Factura erronea'),
+        ('invoice_dateless', 'Factura fuera de tiempo'),
+        ('other','Otros')], default='bad_invoice', string='Motivo de cancelación', copy=False)
+
+    motive = fields.Text(string='Motivo de cancelacion')
